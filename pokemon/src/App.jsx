47f4333f7f5a74535a12d5cbe39bdabfabceb0e7
Javier Campocosio <jcampocosio@gmail.com>
2024-04-29 11:44:19 +0200

    tarjeta centrada y con estructura creada

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -17,25 +17,28 @@ export default function App() {
   return (
     <>
       {pokemon && (
-        <div class="">
-          <img src={pokemon?.sprites.other.showdown.front_default} alt="pokemon"></img>
+        <div class="bg-white flex flex-col items-center">
+          <img class="rounded-full w-40 h-auto mb-6" src={pokemon?.sprites.other.showdown.front_default} alt="pokemon"></img>
 
-          <div class="flex">
+          <div class="flex mx-auto items-center">
             <p class="font-bold">{pokemon?.name}</p>
-            <p>{pokemon?.stats[0].base_stat}hp</p>
+            <p class="text-gray-400 mb-1">{pokemon?.stats[0].base_stat}hp</p>
           </div>
           <div>
-            exp: {pokemon?.base_experience}
+            <p class="text-gray-400 mb-4">{pokemon?.base_experience} exp</p>
           </div>
-          <div>
-            <div>
-              Attack: {pokemon?.stats[1].base_stat}
+          <div class="flex border-t border-gray-400 ">
+            <div class="mx-3 text-center">
+              <p class="font-bold">{pokemon?.stats[1].base_stat}</p>
+              <p class="text-gray-500">Attack</p>
             </div>
-            <div>
-              Special-Attack: {pokemon?.stats[3].base_stat}
+            <div class="mx-3 text-center">
+              <p class="font-bold">{pokemon?.stats[3].base_stat}</p>
+              <p class="text-gray-500">Special-Attack</p>
             </div>
-            <div>
-              Defense: {pokemon?.stats[2].base_stat}
+            <div class="mx-3 text-center">
+              <p class="font-bold">{pokemon?.stats[2].base_stat}</p>
+              <p class="text-gray-500">Defense</p>
             </div>
           </div>
         </div>
