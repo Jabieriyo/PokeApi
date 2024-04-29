813707148aa33f9a7610f8bb8fa777df14dd7ec0
Javier Campocosio <jcampocosio@gmail.com>
2024-04-29 10:17:01 +0200

    devolver informcion pokemon

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,20 +1,45 @@
-import { useEffect, useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { obtenerPokemon } from "./ObtenerPokemon";
 
 export default function App() {
-  const [infoPokemon, setInfoPokemon] = useState("")
+  const [pokemon, setPokemon] = useState(null);
 
-  const obtenerPokemon = () => {
-
-    const id = Math.floor(Math.random() * 150) + 1
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        setInfoPokemon(data)
-      })
-  }
   useEffect(() => {
-    obtenerPokemon()
-  }, [])
+    const getPokemon = async () => {
+      const pokemonId = obtenerPokemon();
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+      const data = await response.json();
+      setPokemon(data);
+    };
+    getPokemon();
+  }, []);
+
+  return (
+    <>
+      {pokemon && (
+        <div class="">
+          <img src={pokemon?.sprites.other.showdown.front_default} alt="pokemon"></img>
+
+          <div class="flex">
+            <p class="font-bold">{pokemon?.name}</p>
+            <p>{pokemon?.stats[0].base_stat}hp</p>
+          </div>
+          <div>
+            exp: {pokemon?.base_experience}
+          </div>
+          <div>
+            <div>
+              Attack: {pokemon?.stats[1].base_stat}
+            </div>
+            <div>
+              Special-Attack: {pokemon?.stats[3].base_stat}
+            </div>
+            <div>
+              Defense: {pokemon?.stats[2].base_stat}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
---
+++ pokemon/src/ObtenerPokemon.jsx
@@ -0,0 +1,4 @@
+export const obtenerPokemon = () => {
+    const id = Math.floor(Math.random() * 150) + 1
+    return id;
+}
